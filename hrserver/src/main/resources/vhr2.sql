f8ae663e869604b7f2ebdeca5bdaf4b4fe9f1bee
xyzhao <xuyz@lianjintai.com>
2019-02-28 13:31:27 +0800

    fix:  建表语句

--- hrserver/src/main/resources/vhr2.sql
+++ hrserver/src/main/resources/vhr2.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50717
  File Encoding         : 65001
 
- Date: 28/02/2019 11:39:43
+ Date: 28/02/2019 13:30:12
 */
 
 SET NAMES utf8mb4;
@@ -58,6 +58,15 @@ CREATE TABLE `hr`  (
 ) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
+-- Records of hr
+-- ----------------------------
+INSERT INTO `hr` VALUES (3, '系统管理员', '18568887789', '029-82881234', '深圳南山', 1, 'admin', '$2a$10$ySG2lkvjFHY5O0./CPIE1OI8VJsuKYEzOYzqIa7AJR6sEgSzUFOAm', 'http://bpic.588ku.com/element_pic/01/40/00/64573ce2edc0728.jpg', NULL);
+INSERT INTO `hr` VALUES (5, '李白', '18568123489', '029-82123434', '海口美兰', 1, 'libai', '$2a$10$oE39aG10kB/rFu2vQeCJTu/V/v4n6DRR0f8WyXRiAYvBpmadoOBE.', 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1514093920321&di=913e88c23f382933ef430024afd9128a&imgtype=0&src=http%3A%2F%2Fp.3761.com%2Fpic%2F9771429316733.jpg', NULL);
+INSERT INTO `hr` VALUES (10, '韩愈', '18568123666', '029-82111555', '广州番禺', 1, 'hanyu', '$2a$10$oE39aG10kB/rFu2vQeCJTu/V/v4n6DRR0f8WyXRiAYvBpmadoOBE.', 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1406745149,1563524794&fm=27&gp=0.jpg', NULL);
+INSERT INTO `hr` VALUES (11, '柳宗元', '18568123377', '029-82111333', '广州天河', 1, 'liuzongyuan', '$2a$10$oE39aG10kB/rFu2vQeCJTu/V/v4n6DRR0f8WyXRiAYvBpmadoOBE.', 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1515233756&di=0856d923a0a37a87fd26604a2c871370&imgtype=jpg&er=1&src=http%3A%2F%2Fwww.qqzhi.com%2Fuploadpic%2F2014-09-27%2F041716704.jpg', NULL);
+INSERT INTO `hr` VALUES (12, '曾巩', '18568128888', '029-82111222', '广州越秀', 1, 'zenggong', '$2a$10$oE39aG10kB/rFu2vQeCJTu/V/v4n6DRR0f8WyXRiAYvBpmadoOBE.', 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1517070040185&di=be0375e0c3db6c311b837b28c208f318&imgtype=0&src=http%3A%2F%2Fimg2.soyoung.com%2Fpost%2F20150213%2F6%2F20150213141918532.jpg', NULL);
+
+-- ----------------------------
 -- Table structure for hr_role
 -- ----------------------------
 DROP TABLE IF EXISTS `hr_role`;
@@ -71,6 +80,22 @@ CREATE TABLE `hr_role`  (
   CONSTRAINT `hr_role_ibfk_1` FOREIGN KEY (`hrid`) REFERENCES `hr` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT,
   CONSTRAINT `hr_role_ibfk_2` FOREIGN KEY (`rid`) REFERENCES `role` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB AUTO_INCREMENT = 50 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of hr_role
+-- ----------------------------
+INSERT INTO `hr_role` VALUES (1, 3, 6);
+INSERT INTO `hr_role` VALUES (9, 5, 1);
+INSERT INTO `hr_role` VALUES (10, 5, 4);
+INSERT INTO `hr_role` VALUES (35, 12, 4);
+INSERT INTO `hr_role` VALUES (36, 12, 3);
+INSERT INTO `hr_role` VALUES (37, 12, 2);
+INSERT INTO `hr_role` VALUES (43, 11, 3);
+INSERT INTO `hr_role` VALUES (44, 11, 2);
+INSERT INTO `hr_role` VALUES (45, 11, 4);
+INSERT INTO `hr_role` VALUES (46, 11, 5);
+INSERT INTO `hr_role` VALUES (48, 10, 3);
+INSERT INTO `hr_role` VALUES (49, 10, 4);
 
 -- ----------------------------
 -- Table structure for menu
@@ -93,6 +118,38 @@ CREATE TABLE `menu`  (
 ) ENGINE = InnoDB AUTO_INCREMENT = 29 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
+-- Records of menu
+-- ----------------------------
+INSERT INTO `menu` VALUES (1, '/', NULL, NULL, '所有', NULL, NULL, NULL, NULL, 1);
+INSERT INTO `menu` VALUES (2, '/', '/home', 'Home', '客户资料', 'fa fa-user-circle-o', NULL, 1, 1, 1);
+INSERT INTO `menu` VALUES (3, '/', '/home', 'Home', '人事管理', 'fa fa-address-card-o', NULL, 1, 1, 1);
+INSERT INTO `menu` VALUES (4, '/', '/home', 'Home', '薪资管理', 'fa fa-money', NULL, 1, 1, 1);
+INSERT INTO `menu` VALUES (5, '/', '/home', 'Home', '统计管理', 'fa fa-bar-chart', NULL, 1, 1, 1);
+INSERT INTO `menu` VALUES (6, '/', '/home', 'Home', '系统管理', 'fa fa-windows', NULL, 1, 1, 1);
+INSERT INTO `menu` VALUES (7, '/employee/basic/**', '/emp/basic', 'EmpBasic', '基本资料', NULL, NULL, 1, 2, 1);
+INSERT INTO `menu` VALUES (8, '/employee/advanced/**', '/emp/adv', 'EmpAdv', '高级资料', NULL, NULL, 1, 2, 0);
+INSERT INTO `menu` VALUES (9, '/personnel/emp/**', '/per/emp', 'PerEmp', '员工资料', NULL, NULL, 1, 3, 0);
+INSERT INTO `menu` VALUES (10, '/personnel/ec/**', '/per/ec', 'PerEc', '员工奖惩', NULL, NULL, 1, 3, 1);
+INSERT INTO `menu` VALUES (11, '/personnel/train/**', '/per/train', 'PerTrain', '员工培训', NULL, NULL, 1, 3, 1);
+INSERT INTO `menu` VALUES (12, '/personnel/salary/**', '/per/salary', 'PerSalary', '员工调薪', NULL, NULL, 1, 3, 1);
+INSERT INTO `menu` VALUES (13, '/personnel/remove/**', '/per/mv', 'PerMv', '员工调动', NULL, NULL, 1, 3, 1);
+INSERT INTO `menu` VALUES (14, '/salary/sob/**', '/sal/sob', 'SalSob', '工资账套管理', NULL, NULL, 1, 4, 1);
+INSERT INTO `menu` VALUES (15, '/salary/sobcfg/**', '/sal/sobcfg', 'SalSobCfg', '员工账套设置', NULL, NULL, 1, 4, 1);
+INSERT INTO `menu` VALUES (16, '/salary/table/**', '/sal/table', 'SalTable', '工资表管理', NULL, NULL, 1, 4, 1);
+INSERT INTO `menu` VALUES (17, '/salary/month/**', '/sal/month', 'SalMonth', '月末处理', NULL, NULL, 1, 4, 1);
+INSERT INTO `menu` VALUES (18, '/salary/search/**', '/sal/search', 'SalSearch', '工资表查询', NULL, NULL, 1, 4, 1);
+INSERT INTO `menu` VALUES (19, '/statistics/all/**', '/sta/all', 'StaAll', '综合信息统计', NULL, NULL, 1, 5, 1);
+INSERT INTO `menu` VALUES (20, '/statistics/score/**', '/sta/score', 'StaScore', '员工积分统计', NULL, NULL, 1, 5, 1);
+INSERT INTO `menu` VALUES (21, '/statistics/personnel/**', '/sta/pers', 'StaPers', '人事信息统计', NULL, NULL, 1, 5, 1);
+INSERT INTO `menu` VALUES (22, '/statistics/recored/**', '/sta/record', 'StaRecord', '人事记录统计', NULL, NULL, 1, 5, 1);
+INSERT INTO `menu` VALUES (23, '/system/basic/**', '/sys/basic', 'SysBasic', '基础信息设置', NULL, NULL, 1, 6, 1);
+INSERT INTO `menu` VALUES (24, '/system/cfg/**', '/sys/cfg', 'SysCfg', '系统管理', NULL, NULL, 1, 6, 1);
+INSERT INTO `menu` VALUES (25, '/system/log/**', '/sys/log', 'SysLog', '操作日志管理', NULL, NULL, 1, 6, 1);
+INSERT INTO `menu` VALUES (26, '/system/hr/**', '/sys/hr', 'SysHr', '操作员管理', NULL, NULL, 1, 6, 1);
+INSERT INTO `menu` VALUES (27, '/system/data/**', '/sys/data', 'SysData', '备份恢复数据库', NULL, NULL, 1, 6, 1);
+INSERT INTO `menu` VALUES (28, '/system/init/**', '/sys/init', 'SysInit', '初始化数据库', NULL, NULL, 1, 6, 1);
+
+-- ----------------------------
 -- Table structure for menu_role
 -- ----------------------------
 DROP TABLE IF EXISTS `menu_role`;
@@ -108,6 +165,43 @@ CREATE TABLE `menu_role`  (
 ) ENGINE = InnoDB AUTO_INCREMENT = 278 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
+-- Records of menu_role
+-- ----------------------------
+INSERT INTO `menu_role` VALUES (161, 7, 3);
+INSERT INTO `menu_role` VALUES (162, 7, 6);
+INSERT INTO `menu_role` VALUES (247, 7, 4);
+INSERT INTO `menu_role` VALUES (248, 8, 4);
+INSERT INTO `menu_role` VALUES (249, 11, 4);
+INSERT INTO `menu_role` VALUES (250, 7, 2);
+INSERT INTO `menu_role` VALUES (251, 8, 2);
+INSERT INTO `menu_role` VALUES (252, 9, 2);
+INSERT INTO `menu_role` VALUES (253, 10, 2);
+INSERT INTO `menu_role` VALUES (254, 12, 2);
+INSERT INTO `menu_role` VALUES (255, 13, 2);
+INSERT INTO `menu_role` VALUES (256, 7, 1);
+INSERT INTO `menu_role` VALUES (257, 8, 1);
+INSERT INTO `menu_role` VALUES (258, 9, 1);
+INSERT INTO `menu_role` VALUES (259, 10, 1);
+INSERT INTO `menu_role` VALUES (260, 11, 1);
+INSERT INTO `menu_role` VALUES (261, 12, 1);
+INSERT INTO `menu_role` VALUES (262, 13, 1);
+INSERT INTO `menu_role` VALUES (263, 14, 1);
+INSERT INTO `menu_role` VALUES (264, 15, 1);
+INSERT INTO `menu_role` VALUES (265, 16, 1);
+INSERT INTO `menu_role` VALUES (266, 17, 1);
+INSERT INTO `menu_role` VALUES (267, 18, 1);
+INSERT INTO `menu_role` VALUES (268, 19, 1);
+INSERT INTO `menu_role` VALUES (269, 20, 1);
+INSERT INTO `menu_role` VALUES (270, 21, 1);
+INSERT INTO `menu_role` VALUES (271, 22, 1);
+INSERT INTO `menu_role` VALUES (272, 23, 1);
+INSERT INTO `menu_role` VALUES (273, 24, 1);
+INSERT INTO `menu_role` VALUES (274, 25, 1);
+INSERT INTO `menu_role` VALUES (275, 26, 1);
+INSERT INTO `menu_role` VALUES (276, 27, 1);
+INSERT INTO `menu_role` VALUES (277, 28, 1);
+
+-- ----------------------------
 -- Table structure for role
 -- ----------------------------
 DROP TABLE IF EXISTS `role`;
@@ -119,47 +213,15 @@ CREATE TABLE `role`  (
 ) ENGINE = InnoDB AUTO_INCREMENT = 15 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Procedure structure for addDep
+-- Records of role
 -- ----------------------------
-DROP PROCEDURE IF EXISTS `addDep`;
-delimiter ;;
-CREATE PROCEDURE `addDep`(in depName varchar(32),in parentId int,in enabled boolean,out result int,out result2 int)
-begin
-  declare did int;
-  declare pDepPath varchar(64);
-  insert into department set name=depName,parentId=parentId,enabled=enabled;
-  select row_count() into result;
-  select last_insert_id() into did;
-  set result2=did;
-  select depPath into pDepPath from department where id=parentId;
-  update department set depPath=concat(pDepPath,'.',did) where id=did;
-  update department set isParent=true where id=parentId;
-end
-;;
-delimiter ;
-
--- ----------------------------
--- Procedure structure for deleteDep
--- ----------------------------
-DROP PROCEDURE IF EXISTS `deleteDep`;
-delimiter ;;
-CREATE PROCEDURE `deleteDep`(in did int,out result int)
-begin
-  declare ecount int;
-  declare pid int;
-  declare pcount int;
-  select count(*) into ecount from employee where departmentId=did;
-  if ecount>0 then set result=-1;
-  else 
-  select parentId into pid from department where id=did;
-  delete from department where id=did and isParent=false;
-  select row_count() into result;
-  select count(*) into pcount from department where parentId=pid;
-  if pcount=0 then update department set isParent=false where id=pid;
-  end if;
-  end if;
-end
-;;
-delimiter ;
+INSERT INTO `role` VALUES (1, 'ROLE_manager', '部门经理');
+INSERT INTO `role` VALUES (2, 'ROLE_personnel', '人事专员');
+INSERT INTO `role` VALUES (3, 'ROLE_recruiter', '招聘主管');
+INSERT INTO `role` VALUES (4, 'ROLE_train', '培训主管');
+INSERT INTO `role` VALUES (5, 'ROLE_performance', '薪酬绩效主管');
+INSERT INTO `role` VALUES (6, 'ROLE_admin', '系统管理员');
+INSERT INTO `role` VALUES (13, 'ROLE_test2', '测试角色2');
+INSERT INTO `role` VALUES (14, 'ROLE_test1', '测试角色1');
 
 SET FOREIGN_KEY_CHECKS = 1;
